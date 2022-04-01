4514e048d27c3c06fa384680b858f4515c28e514
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-01 11:48:01 +0300

    App Completed.

--- app/src/main/java/com/metehanbolat/pdfviewercompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/pdfviewercompose/MainActivity.kt
@@ -3,14 +3,65 @@ package com.metehanbolat.pdfviewercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.pdfviewercompose.ui.theme.PDFViewerComposeTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PDFViewerComposeTheme {
+                ComposePDFViewer()
+            }
+        }
+    }
+}
 
+@ExperimentalFoundationApi
+@Composable
+fun ComposePDFViewer() {
+    var isLoading by remember { mutableStateOf(false) }
+    var currentLoadingPage by remember { mutableStateOf<Int?>(null) }
+    var pageCount by remember { mutableStateOf<Int?>(null) }
+    
+    Box {
+        PdfViewer(
+            pdfResId = R.raw.earth,
+            modifier = Modifier.fillMaxSize(),
+            loadingListener = { loading, currentPage, maxPage ->
+                isLoading = loading
+                if (currentPage != null) currentLoadingPage = currentPage
+                if (maxPage != null) pageCount = maxPage
+            }
+        )
+        if (isLoading) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp),
+                    progress = if (currentLoadingPage == null || pageCount == null) 0f
+                else currentLoadingPage!!.toFloat() / pageCount!!.toFloat()
+                )
+
+                Text(
+                    text = "${currentLoadingPage ?: "-"} pages loaded / ${pageCount ?: "-"} total pages",
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(top = 5.dp)
+                        .padding(horizontal = 30.dp)
+                )
             }
         }
     }
